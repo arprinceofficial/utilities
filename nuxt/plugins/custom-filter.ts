0ed4f7877c6c890da9a5d8f0790bf2aabc65eb46
Md. Ashiqur Rahman <ashiqurr04@gmail.com>
2024-10-17 14:26:18 +0600

    Update custom-filter.ts

--- nuxt/plugins/custom-filter.ts
+++ nuxt/plugins/custom-filter.ts
@@ -141,6 +141,32 @@ export default defineNuxtPlugin(() => {
                 if (invalidChars.includes(chr) || !validChars.includes(chr)) return true
                 else return false
             },
+            invalidYear: (e: any) => {
+                var invalidChars = ["-", "+", "e", "E"];
+                var validChars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+                var validLength = 4;
+                var inputLength = e.target.value.length;
+
+                let inputValue = e.target.value + e.key;
+                let year = Number(inputValue);
+                if (inputValue.length >= 2 && !(inputValue.startsWith("19") || inputValue.startsWith("20") || inputValue.startsWith("21"))) {
+                    e.preventDefault();
+                    return;
+                }
+                if (inputValue.length === 4 && (year < 1900 || year > 2100)) {
+                    e.preventDefault();
+                    return;
+                }
+
+                if (inputLength >= validLength) {
+                    e.preventDefault();
+                    return;
+                }
+                if (invalidChars.includes(e.key) || !validChars.includes(e.key)) {
+                    e.preventDefault();
+                    return;
+                }
+            },
             invalidChr: (reg_exp: string, chr: any) => {
                 const re = new RegExp(`${reg_exp}`, 'g')
                 // console.log(re)
