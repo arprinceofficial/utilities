2797ddbbf80e8cbdc5c43088975ecb073f30d8bc
Md. Ashiqur Rahman <ashiqurr04@gmail.com>
2024-10-17 14:04:06 +0600

    Update regular_use_case.js

--- js/regular_use_case.js
+++ js/regular_use_case.js
@@ -1,1 +1,2 @@
+// <div class="text-center ml-auto">{{ total_property_assessed ? data.total_property_assessed.toLocaleString("en-US", { minimumFractionDigits: 2, maximumFractionDigits: 2 }) : "0.00" }}</div>
 total_property_assessed.toLocaleString("en-US", { minimumFractionDigits: 2, maximumFractionDigits: 2
