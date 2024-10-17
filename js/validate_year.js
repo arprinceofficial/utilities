8e7cfc5e0ea81a76de52f82a6ff1d0e3f1eed844
Md. Ashiqur Rahman <ashiqurr04@gmail.com>
2024-10-17 13:23:48 +0600

    Create validate_year.js

---
+++ js/validate_year.js
@@ -0,0 +1,26 @@
+const validate_year = (e) => {
+        var invalidChars = ["-", "+", "e", "E"];
+        var validChars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+        var validLength = 4;
+        var inputLength = e.target.value.length;
+
+        let inputValue = e.target.value + e.key;
+        let year = Number(inputValue);
+        if (inputValue.length >= 2 && !(inputValue.startsWith("19") || inputValue.startsWith("20") || inputValue.startsWith("21"))) {
+            e.preventDefault();
+            return;
+        }
+        if (inputValue.length === 4 && (year < 1900 || year > 2100)) {
+            e.preventDefault();
+            return;
+        }
+
+        if (inputLength >= validLength) {
+            e.preventDefault();
+            return;
+        }
+        if (invalidChars.includes(e.key) || !validChars.includes(e.key)) {
+            e.preventDefault();
+            return;
+        }
+    };
