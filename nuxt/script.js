e61f3abb615c447623f047a50a009779bce7d0a3
Md. Ashiqur Rahman <ashiqurr04@gmail.com>
2024-11-20 11:07:36 +0600

    Update script.js

--- nuxt/script.js
+++ nuxt/script.js
@@ -4,3 +4,23 @@ useHead({
         .map(word => word.charAt(0).toUpperCase() + word.slice(1))
         .join(' ')
 });
+
+
+*** useState ***
+==============
+Declare =>
+const get_lang_translation = useState('get_lang_translation', () => [])
+==============
+Store =>
+get_lang_translation.value = res.data.language_translation
+==============
+Use Case =>
+const get_lang_translation = useState('get_lang_translation')
+
+============== Array use case ==============
+if (Array.isArray(get_lang_translation.value)) {
+                    const translation = get_lang_translation.value.find((item: any) => item.language_short_code === short_code)
+                    return translation ? translation.content : '';
+                    // return translation ? translation.content : short_code;
+                }
+                return short_code;
