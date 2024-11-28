0745a8f702ca944b29ea0a3c125ed937a39a23b3
Md. Ashiqur Rahman <ashiqurr04@gmail.com>
2024-11-28 09:54:20 +0600

    Update custom-filter.ts

--- nuxt/plugins/custom-filter.ts
+++ nuxt/plugins/custom-filter.ts
@@ -134,6 +134,62 @@ export default defineNuxtPlugin(() => {
 
                 return str;
             },
+            toWordsWithPoints: (s: any) => {
+                // American Numbering System
+                var th = ['', 'thousand', 'million', 'billion', 'trillion'];
+                var dg = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+                var tn = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+                var tw = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+            
+                s = s.toString();
+                s = s.replace(/[\, ]/g, '');
+                if (s != parseFloat(s)) return 'not a number';
+            
+                var x = s.indexOf('.');
+                if (x == -1) x = s.length;
+                if (x > 15) return 'too big';
+            
+                var n = s.split('');
+                var str = '';
+                var sk = 0;
+            
+                // Process whole number part
+                for (let i = 0; i < x; i++) {
+                    if ((x - i) % 3 == 2) {
+                        if (n[i] == '1') {
+                            str += tn[Number(n[i + 1])] + ' ';
+                            i++;
+                            sk = 1;
+                        } else if (n[i] != 0) {
+                            str += tw[n[i] - 2] + ' ';
+                            sk = 1;
+                        }
+                    } else if (n[i] != 0) {
+                        str += dg[n[i]] + ' ';
+                        if ((x - i) % 3 == 0) str += 'hundred ';
+                        sk = 1;
+                    }
+                    if ((x - i) % 3 == 1) {
+                        if (sk) str += th[(x - i - 1) / 3] + ' ';
+                        sk = 0;
+                    }
+                }
+            
+                str += 'PHP'; // Add currency
+            
+                // Process decimal (Paisa) part
+                if (x != s.length) {
+                    var paisaPart = '';
+                    for (let i = x + 1; i < s.length; i++) {
+                        paisaPart += dg[Number(n[i])] + ' ';
+                    }
+                    if (paisaPart.trim() !== 'zero zero') { // Only add Paisa if not 0
+                        str += ' and ' + paisaPart.trim() + ' Peso';
+                    }
+                }
+            
+                return str.trim();
+            },
             invalidNumber: (chr: any) => {
                 var invalidChars = ["-","+","e","E"]
                 var validChars = ['0','1','2','3','4','5','6','7','8','9','.']
