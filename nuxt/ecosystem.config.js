99ecf4bf7415597ab2110cb2f6140c7805506293
Md. Ashiqur Rahman <ashiqurr04@gmail.com>
2025-01-19 01:07:18 +0600

    Create ecosystem.config.js

---
+++ nuxt/ecosystem.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  apps: [
+    {
+      name: 'book-store',
+      exec_mode: 'cluster',
+      instances: '1',
+      env: {
+        PORT: 30012,
+        NITRO_HOST: '127.0.0.1',
+        NODE_ENV: 'production',
+      },
+      script: './.output/server/index.mjs',
+    },
+  ],
+};
