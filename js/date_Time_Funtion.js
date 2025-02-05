ff6781704fa0c5043eab1a2c664665d66f1b41fb
Md. Ashiqur Rahman <ashiqurr04@gmail.com>
2025-02-05 10:35:36 +0600

    Update date_Time_Funtion.js

--- js/date_Time_Funtion.js
+++ js/date_Time_Funtion.js
@@ -1,3 +1,14 @@
+const dateTime = (date) => {
+    return new Date(date).toLocaleString("en-US", {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric"
+    })
+}
+
 const convert = (str) => {
     var date = new Date(str),
         mnth = ("0" + (date.getMonth() + 1)).slice(-2),
